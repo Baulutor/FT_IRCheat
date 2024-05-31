413eb8593184b2b96b5f7d4a49d5d83858de631c
dbaule <dbaule@student.42lyon.fr>
2024-05-31 19:05:04 +0200

    merge ben

--- srcs/cmd/Quit.cpp
+++ srcs/cmd/Quit.cpp
@@ -4,7 +4,9 @@
 
 #include "Cmd.hpp"
 
-void 	Quit(std::string cmd, Clients& client, Server& server)
+#include "Cmd.hpp"
+
+void Quit(std::string cmd, Clients& client, Server& server)
 {
 
 	std::cout << "["<< cmd << "]" <<  std::endl;
@@ -15,7 +17,63 @@ void 	Quit(std::string cmd, Clients& client, Server& server)
 	for (; chan != channels.end(); chan++)
 	{
 		std::map<int, Clients> clientMap = chan->second.getClientMap();
-//		std::map<std::string, Clients>::iterator ok = clientMap.find();
+		std::map<int, Clients> clientInvitedMap = chan->second.getClientInvited();
+		std::map<int, Clients>::iterator ok = clientMap.find(client.getFd());
+		std::vector<Clients> opeVec = chan->second.getOperatorVector();
+//		for (size_t i = 0; i < opeVec.size(); i++)
+//		{
+//			std::cout << opeVec[i].getFd() << ": c'est les differents frero dans le bail operateur !!!!!" << std::endl;
+//		}
+		std::cout << "chan->first: " << chan->first << " , valeur de opeVec.size(): " << opeVec.size() << std::endl;
+		if (opeVec.size() == 1)
+		{
+			if (opeVec.begin()->getFd() == client.getFd())
+			{
+				if (clientMap.size() == 1)
+				{
+					std::cout << "cas de channel seul !" << std::endl;
+					client.getChannelsClient().find(chan->first)->second.getClientInvited().clear();
+					client.getChannelsClient().find(chan->first)->second.getClientMap().clear();
+					client.getChannelsClient().find(chan->first)->second.getOperatorVector().clear();
+					client.getChannelsClient().clear();
+					client.getChannelsInvite().clear();
+					return ;
+				}
+				else
+				{
+					for (std::map<int, Clients>::iterator it = clientMap.begin(); it != clientMap.end(); it++)
+					{
+						if (it->first != client.getFd())
+						{
+//client.getChannels().find(chan->first)->second.setOperator(it->second);
+							break ;
+						}
+					}
+					client.getChannelsClient().find(chan->first)->second.removeOperator(client);
+				}
+			}
+		}
+		server.getChannels().find(chan->first)->second.getClientMap().erase(ok);
+		std::map<int, Clients>::iterator invitedMapIt = server.getChannels().find(chan->first)->second.getClientInvited().find(client.getFd());
+		if (invitedMapIt != clientInvitedMap.end())
+		{
+			if (clientInvitedMap.size() == 1)
+				server.getChannels().find(chan->first)->second.getClientInvited().clear();
+			else
+				server.getChannels().find(chan->first)->second.getClientInvited().erase(invitedMapIt);
+		}
+		int fdClient = client.getFd();
+		std::vector<pollfd> pollFdServ = server.getLstPollFd();
+		for (size_t i = 0; i < pollFdServ.size() - 1; i++)
+		{
+			if (pollFdServ[i].fd == fdClient)
+			{
+				close(server.getLstPollFd().at(i).fd);
+				std::vector<pollfd>::iterator it = pollFdServ.begin() + i;
+				server.getLstPollFd().erase(it);
+				break ;
+			}
+		}
 	}
 
 	(void)client;
